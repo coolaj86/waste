870ad4c980481e017e0ead83b4600a845a2c08a9
AJ ONeal <coolaj86@gmail.com>
2013-12-19 19:02:47 -0700

    a login properly creates an account

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -1,9 +1,8 @@
 'use strict';
 
 angular.module('sortinghatApp')
-  .controller('LoginCtrl', function ($modalInstance, StSession, mySession) {
+  .controller('LoginCtrl', function ($modalInstance, StLogin) {
     var $scope = this
-      , login = {}
       ;
 
     // Crazy window open/close hacks and mobile chrome on iOS workarounds
@@ -12,55 +11,16 @@ angular.module('sortinghatApp')
     //
     // Facebook
     //
-    function makeLogin(abbr, authUrl) {
-      var uAbbr = abbr.replace(/(^.)/, function ($1) { return $1.toUpperCase(); })
-        ;
-
-      login['poll' + uAbbr + 'Login'] = function () {
-        //jQuery('body').append('<p>' + 'heya' + '</p>');
-        if (!window.localStorage) {
-          clearInterval(login['poll' + uAbbr + 'Int']);
-          // doomed!
-          return;
-        }
-
-        if (localStorage.getItem(abbr + 'Status')) {
-          window['complete' + uAbbr + 'Login'](localStorage.getItem(abbr + 'Status'));
-        }
-      };
-      window['complete' + uAbbr + 'Login'] = function (url, accessToken, email, link) {
-        clearInterval(login['poll' + uAbbr + 'Int']);
-        localStorage.removeItem(abbr + 'Status');
-        login.loginCallback();
-
-        console.log('accessed parent function complete' + uAbbr + 'Login', accessToken, email, link);
-        //jQuery('body').append('<p>' + url + '</p>');
-        login.loginWindow.close();
-        delete login.loginWindow;
-      };
-      $scope['loginWith' + uAbbr + ''] = function () {
-        console.log('loginWith' + uAbbr + '');
-        login.loginCallback = function () {
-          console.log('loginCallback');
-          StSession.read({ expire: true }).then(function (session) {
-            console.log('StSession.read()', session);
-            if (session.error) {
-              StSession.destroy();
-            }
-            $modalInstance.close(session);
-          });
-        };
-        login.loginWindow = window.open(authUrl);
-        login['poll' + uAbbr + 'Int'] = setInterval(login['poll' + uAbbr + 'Login'], 300);
-      };
-    }
-    makeLogin('fb', '/auth/facebook');
+    StLogin.makeLogin($scope, 'fb', '/auth/facebook', function (session) {
+      $modalInstance.close(session);
+    });
 
     //
     // Twitter
     //
-    makeLogin('tw', '/authn/twitter');
-
+    StLogin.makeLogin($scope, 'tw', '/authn/twitter', function (session) {
+      $modalInstance.close(session);
+    });
 
 
     //
@@ -73,6 +33,7 @@ angular.module('sortinghatApp')
     $scope.showEmail = function () {
       $scope.emailShown = true;
     };
+
     $scope.loginWithEmail = function () {
     };
   });
--- scripts/services/st-login.js
+++ scripts/services/st-login.js
@@ -45,7 +45,52 @@ angular.module('sortinghatApp')
       return d.promise;
     }
 
+    function makeLogin(scope, abbr, authUrl, cb) {
+      var uAbbr = abbr.replace(/(^.)/, function ($1) { return $1.toUpperCase(); })
+        , login = {}
+        ;
+
+      login['poll' + uAbbr + 'Login'] = function () {
+        //jQuery('body').append('<p>' + 'heya' + '</p>');
+        if (!window.localStorage) {
+          clearInterval(login['poll' + uAbbr + 'Int']);
+          // doomed!
+          return;
+        }
+
+        if (localStorage.getItem(abbr + 'Status')) {
+          window['complete' + uAbbr + 'Login'](localStorage.getItem(abbr + 'Status'));
+        }
+      };
+      window['complete' + uAbbr + 'Login'] = function (url, accessToken, email, link) {
+        clearInterval(login['poll' + uAbbr + 'Int']);
+        localStorage.removeItem(abbr + 'Status');
+        login.loginCallback();
+
+        console.log('accessed parent function complete' + uAbbr + 'Login', accessToken, email, link);
+        //jQuery('body').append('<p>' + url + '</p>');
+        login.loginWindow.close();
+        delete login.loginWindow;
+      };
+      scope['loginWith' + uAbbr + ''] = function () {
+        console.log('loginWith' + uAbbr + '');
+        login.loginCallback = function () {
+          console.log('loginCallback');
+          StSession.read({ expire: true }).then(function (session) {
+            console.log('StSession.read()', session);
+            if (session.error) {
+              StSession.destroy();
+            }
+            cb(session);
+          });
+        };
+        login.loginWindow = window.open(authUrl);
+        login['poll' + uAbbr + 'Int'] = setInterval(login['poll' + uAbbr + 'Login'], 300);
+      };
+    }
+
     return {
       show: show
+    , makeLogin: makeLogin
     };
   });
