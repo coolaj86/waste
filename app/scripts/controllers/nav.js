cfa66e5dc83722b459dbec31b6393abe8984f421
AJ ONeal <coolaj86@gmail.com>
2014-03-07 10:02:56 -0700

    added logout function

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,9 +1,22 @@
 'use strict';
 
 angular.module('sortinghatApp')
-  .controller('NavCtrl', function ($state, StLogin) {
+  .controller('NavCtrl', function ($state, StLogin, StSession, mySession) {
     var $scope = this
       ;
+
+    function updateSession(session) {
+      if (session && 'guest' !== session.role) {
+        $scope.session = mySession;
+      } else {
+        $scope.session = null;
+      }
+    }
+
+    // XXX this could also be done via dirty checking
+    // using a shared object, but I think
+    // this approach is more familiar to the average programmer
+    StSession.subscribe(updateSession);
 
     $scope.tabs = [
       { active: $state.includes('root')
@@ -18,7 +31,16 @@ angular.module('sortinghatApp')
 
     $scope.login = function () {
       StLogin.show().then(function (data) {
-        $scope.session = data;
+        updateSession(data);
+      }, function () {
+        // nada
+      });
+    };
+
+    $scope.logout = function () {
+      console.log('SNTHSNHT');
+      StSession.destroy().then(function () {
+        updateSession(null);
       }, function () {
         // nada
       });
