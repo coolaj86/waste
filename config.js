f8892122d8973a1d9e759e0cf1648f3ba5c5fd4f
AJ ONeal <coolaj86@gmail.com>
2014-03-08 13:31:06 -0700

    added real fake api keys

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   protocol: 'http'
-, host: 'example.com'
+, host: 'local.foobar3000.com:9003'
 , facebook: {
     // https://developers.facebook.com/apps
     id: 'XXX'
@@ -12,7 +12,13 @@ module.exports = {
   , consumerSecret: 'XXX'
   }
 , ldsconnect: {
-    // https://ldsconnect.org/dev
-    id: 'XXX'
-  , secret: 'XXX'
+    // http://ldsconnect.org
+    id: '55c7-test-bd03'
+  , secret: '6b2fc4f5-test-8126-64e0-b9aa0ce9a50d'
+  }
+, stripe: {
+    // https://manage.stripe.com/account/apikeys
+    id: "pk_test_526DRmZwEOiMxTigV5fX52ti"
+  , secret: "sk_test_Erl9x9947vVPaYigTyKKuXZl"
+  }
 };
