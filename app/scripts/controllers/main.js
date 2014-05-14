046447b9f172f12ae715f3a3cff07731c1387e9b
AJ ONeal <awesome@coolaj86.com>
2014-05-14 10:16:54 -0600

    removed some cruft, fixed login account page bug

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,73 +1,19 @@
 'use strict';
 
 angular.module('sortinghatApp')
-  .controller('MainCtrl', function ($timeout, mySession, data) {
-    var $scope = this
-      , quiz
+  .controller('MainCtrl', function ($scope, $timeout, data, mySession) {
+    var M = this
       ;
 
-    console.log('data');
-    console.log(data);
+    M.message = "This is bound scope, accessed as 'M.message' in templates and 'message' will not leak between scopes";
+    $scope.message = "This is unbound scope, accessed as 'message' in this and child scopes";
 
-    $scope.data = data;
+    // These resources will take some time to resolve.
+    // If the page remains blank without error messages, throw the blame at one of these and check that they resolve
 
-    quiz = window.Quizomatic.create(data, { randomize: true });
+    // Always a guest or null, at the least
+    M.session = mySession;
 
-    $scope.outcomes = data.outcomes;
-
-    function presentQuestion(data) {
-      var current = data.current
-        , remaining = data.remaining
-        , total = data.total
-        ;
-
-      // TODO put at quiz level
-      $scope.total = total;
-
-      $scope.current = current;
-      $scope.current.remaining = remaining;
-      $scope.current.total = total;
-      $scope.current.question = current.question;
-      $scope.current.choices = current.choices;
-    }
-
-    function interpretAnswer(num) {
-      quiz.respond(Number(num));
-
-      if (quiz.hasNext()) {
-        presentQuestion(quiz.next());
-        return;
-      }
-
-      saveResponses();
-    }
-
-    function saveResponses() {
-      var responses = quiz.responses()
-        , totals = quiz.totals()
-        , sorted
-        ;
-
-      $scope.current = null;
-      $scope.results = totals;
-      $scope.responses = responses;
-
-      sorted = Object.keys(totals).sort(function (keyA, keyB) {
-        return totals[keyB] - totals[keyA];
-      });
-      $scope.result = sorted[0];
-      $scope.confidence1 =
-        (100 *
-          (totals[sorted[0]] - totals[sorted[1]])
-        / (totals[sorted[0]] + totals[sorted[1]])
-        ).toFixed(2);
-      $scope.confidence2 =
-        (100 * (totals[sorted[0]] / ($scope.total / (sorted.length/2)) )).toFixed(2);
-    }
-
-    $scope.startQuiz = function () {
-      presentQuestion(quiz.next());
-    };
-
-    $scope.respond = interpretAnswer;
+    // Some data provided by the data service
+    M.data = data;
   });
