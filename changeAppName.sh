edcbcbc3b82ed9d0c087316449945cfb808e9327
AJ ONeal <awesome@coolaj86.com>
2014-05-29 03:31:50 -0600

    updated app name changer

--- changeAppName.sh
+++ changeAppName.sh
@@ -1,3 +1,4 @@
-grep -R sortinghatApp app | cut -d ':' -f1 | while read F; do sed -i '' 's/sortinghatApp/awesomeApp/g' "$F"; done
-grep -R sortinghat app | cut -d ':' -f1 | while read F; do sed -i '' 's/sortinghat/awesome/g' "$F"; done
-grep -R sortinghat bower.json | cut -d ':' -f1 | while read F; do sed -i '' 's/sortinghat/awesome/g' "$F"; done
+#!/bin/bash
+grep -R sortinghatApp app | cut -d ':' -f1 | while read F; do sed -i '' 's/sortinghatApp/yololiumApp/g' "$F"; done
+grep -R sortinghat app | cut -d ':' -f1 | while read F; do sed -i '' 's/sortinghat/yololium/g' "$F"; done
+grep -R sortinghat bower.json | cut -d ':' -f1 | while read F; do sed -i '' 's/sortinghat/yololium/g' "$F"; done
