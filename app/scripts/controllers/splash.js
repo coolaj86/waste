e47279df6b52e4f052592973556c9a5789da5e72
AJ ONeal <awesome@coolaj86.com>
2014-06-14 14:21:27 -0600

    redirect to main page on login

--- app/scripts/controllers/splash.js
+++ app/scripts/controllers/splash.js
@@ -1,8 +1,22 @@
 'use strict';
 
 angular.module('yololiumApp')
-  .controller('SplashCtrl', function ($scope) {
-    var scope = this
-      ;
+  .controller('SplashCtrl', function ($scope, $state, StSession) {
+    var firstTime = true;
 
+    // We don't want to redirect on page load
+    // (the splash page is a restful route)
+    // we just want to redirect if the user logs in
+    function redirect(session) {
+      if (firstTime) {
+        firstTime = false;
+        return;
+      }
+
+      if ('guest' !== session.account.role) {
+        $state.go('root');
+      }
+    }
+
+    StSession.subscribe(redirect, $scope);
   });
