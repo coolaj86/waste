453d4c57af690a69ba0ad7176ab4a752566a2192
AJ ONeal <awesome@coolaj86.com>
2014-07-29 23:59:11 -0700

    session stuff mostly works

--- app/scripts/controllers/account-new.js
+++ app/scripts/controllers/account-new.js
@@ -1,41 +1,77 @@
 'use strict';
 
 angular.module('yololiumApp')
-  .controller('AccountNewCtrl', function ($scope, $http, $timeout, $modalInstance, StAccount, StSession, mySession) {
+  .controller('AccountNewCtrl', function ($scope, $modalInstance, StAccount, StSession, mySession, stAccountOptions, stAccountRequired) {
     var scope = this
-      , account = { loginIds: [] }
       ;
 
-    function init(session) {
-      var defaultAction = 'create';
+    // TODO calculate missing
+    scope.missing = {};
 
+    // This dialog is opened to update necessary account details
+    // It should be passed options to inform the dialog which
+    // missing fields are necessary to show at this time
+    //
+    // Examples:
+    // we want to get facebook but haven't connected yet, so we should show the connection dialog
+    // we just logged in for the first time and don't have an account or a local login
+    function init(session) {
+      // session is always ensured as part of login
       mySession = session;
-      if (scope.account && scope.account.id) {
+
+      var defaultAction = 'create'
+        ;
+
+      scope.account = scope.account || session.account || {};
+      if (scope.account.id) {
         defaultAction = 'update';
       }
+
       scope.accountAction = scope.accountAction || defaultAction;
-      scope.account = {};
-      scope.delta = {};
 
-      /*
-      if ('guest' === session.account.role) {
-        $modalInstance.close(session);
-      }
-      */
+      scope.account = scope.account || {};
+      scope.delta = scope.delta || { localLogin: {} };
+      scope.logins = session.logins.map(function (login) {
+        return {
+          comment: 'local' === login.provider ? (login.uid || 'username') : login.provider
+        , id: login.id
+        , uid: login.uid
+        , provider: login.provider
+        , type: login.type
+        , link: true
+        };
+      });
 
-      session.accounts.some(function (a) {
-        if (a.id === session.selectedAccountId) {
-          account =  a;
+      console.log('[new account] session.logins');
+      console.log(session.logins);
+      session.logins.some(function (login) {
+        if ('local' === (login.type || login.provider)) {
+          scope.account.localLoginId = login.id;
+          scope.delta.localLogin.id = login.id;
         }
       });
 
+      // login is always ensured prior to account
       if (session.login.emails) {
         scope.delta.email = session.login.emails[0] || {};
         scope.delta.email = scope.delta.email.value;
       }
+      if (!scope.delta.email) {
+        session.logins.some(function (login) {
+          scope.delta.email = (login.emails[0] || {}).value;
+          return scope.delta.email;
+        });
+      }
 
+      session.account = session.account || {};
       Object.keys(session.account).forEach(function (field) {
         scope.account[field] = session.account[field];
+      });
+
+      stAccountRequired.forEach(function (r) {
+        if (!scope.account[r]) {
+          scope.missing[r] = true;
+        }
       });
     }
     init(mySession);
@@ -44,30 +80,10 @@ angular.module('yololiumApp')
     scope.updateAccount = function () {
       console.log('update account');
       console.log(scope.delta);
+      scope.delta.logins = scope.logins.filter(function (l) { return l.link; });
       StAccount.update(mySession.selectedAccountId, scope.delta).then(function (session) {
         console.log('UPDATE 1');
         console.log(session);
-        /*
-        var account = session && session.account
-          ;
-
-        if (!account || !account.id || account.error) {
-          console.error('ERROR updating account');
-          console.error(account);
-          return;
-        }
-
-        // TODO do these account adjustments in StSession
-        if (!mySession.accounts.some(function (a) {
-          return a.id === account.id;
-        })) {
-          mySession.accounts.push(account);
-        }
-        mySession.selectedAccountId = account.id;
-        */
-
-        // TODO could probably just update the session in this scope
-        // instead of using StSession.update to broacast
         StSession.update(session);
 
         $modalInstance.close(session);
