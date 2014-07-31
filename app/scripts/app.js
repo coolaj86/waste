a4fa25b29373af5944f8b76565759ac3dad13e56
AJ ONeal <awesome@coolaj86.com>
2014-07-30 22:33:05 -0700

    always use camelCase for receiving from our API on the browser side

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@ angular.module('yololiumApp', [
   'ui.bootstrap',
   'steve'
 ])
-  .config(function ($stateProvider, $urlRouterProvider/*, stConfig*/) {
+  .config(function ($stateProvider, $urlRouterProvider, $httpProvider/*, stConfig*/) {
     var nav
       , footer
       ;
@@ -252,6 +252,45 @@ angular.module('yololiumApp', [
       })
       */
       ;
+
+    // alternatively, register the interceptor via an anonymous factory
+    $httpProvider.interceptors.push(function(/*$q*/) {
+      var recase = window.Recase.create({ exceptions: {} })
+        ;
+
+      return {
+        'request': function (config) {
+          if (!/.html/.test(config.url)) {
+            //console.log('[$http] request');
+            //console.log(config);
+            //console.log(config.method, config.url);
+          }
+          return config;
+        }
+      , 'requestError': function (rejection) {
+          //console.log('[$http] requestError');
+          //console.log(rejection);
+          return rejection;
+        }
+      , 'response': function (response) {
+          var config = response.config
+            ;
+
+          // our own API is snake_case (to match webApi / ruby convention)
+          // but we convert to camelCase for javascript convention
+          if (!/^https?:\/\//.test(config.url) && /json/.test(response.headers('Content-Type'))) {
+            response.data = recase.camelCopy(response.data);
+          }
+          return response;
+        }
+      , 'responseError': function (rejection) {
+          //console.log('[$http] responseError');
+          //console.log(rejection);
+          return rejection;
+        }
+
+      };
+    });
   })
   .run(function ($rootScope, $state, StSession) {
     var currentSession
