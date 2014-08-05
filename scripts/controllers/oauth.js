bebafa9be6922de9256e456b56b3f0e548334e23
AJ ONeal <awesome@coolaj86.com>
2014-08-05 04:13:46 -0700

    success case almost works completely

--- scripts/controllers/oauth.js
+++ scripts/controllers/oauth.js
@@ -8,15 +8,39 @@
  * Controller of the yololiumApp
  */
 angular.module('yololiumApp')
-  .controller('OauthCtrl', function ($scope, $http, $stateParams, StSession) {
+  .controller('OauthCtrl', function ($scope, $http, $stateParams, StSession, StApi) {
     var scope = this
       ;
 
-    return;
-    StSession.ensureSession().then(function (session) {
+    console.log("$stateParams");
+    console.log($stateParams);
+
+    StSession.ensureSession(
+      // role
+      null
+      // TODO login opts (these are hypothetical)
+    , { close: false
+      , options: ['login', 'create']
+      , default: 'login'
+      , includeAccount: true // show the account stuff when asking to create a login
+      }
+      // TODO account opts
+    , { verify: ['email', 'phone']
+      }
+    ).then(function (session) {
       console.log(session);
       console.log($stateParams);
       // get token from url param
       scope.token = $stateParams.token;
+      $http.get(StApi.oauthPrefix + '/scope/' + $stateParams.token).then(function (resp) {
+        console.log("resp.data");
+        console.log(resp.data);
+        scope.invalids = resp.data.invalids;
+        scope.scope = resp.data.scope.map(function (name) {
+          return { accepted: true, name: name };
+        });
+        scope.redirectUrl = resp.data.url;
+        scope.transactionId = resp.data.transactionId;
+      });
     });
   });
