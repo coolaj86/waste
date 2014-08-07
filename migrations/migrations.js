2aa44541f9754f6b6d22dc3c32b567b2667f93cf
L8D <tenorbiel@gmail.com>
2014-08-07 16:57:09 -0500

    Added contacts_id field to accounts

--- migrations/migrations.js
+++ migrations/migrations.js
@@ -4,4 +4,5 @@ module.exports = [
   require('./2014-07-14_03-43-23_data.json')
 , require('./2014-07-24_10-42-43_logins-and-accounts.json')
 , require('./2014-08-05_16-37-57_oauth_clients.json')
+, require('./2014-08-07-16-04-23_contacts.json')
 ];
