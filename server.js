9c9060be74d3408fba215e8c896a45ab95b96d95
AJ ONeal <awesome@coolaj86.com>
2014-08-07 17:09:52 -0700

    added missing res.error

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ function init(Db) {
     }))
     .use(require('compression')())
     .use(require('./lib/connect-shims/redirect'))
-    .use(require('connect-send-error').json())
+    .use(require('connect-send-error').error())
     .use(require('connect-send-json').json())
     ;
 
