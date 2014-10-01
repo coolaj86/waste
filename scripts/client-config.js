d6bbb408190fc3783f0cc1f076fbc3c9398f0d6f
AJ ONeal <awesome@coolaj86.com>
2014-10-01 16:09:57 -0400

    steps towards making config.js restfully-editable

---
+++ scripts/client-config.js
@@ -0,0 +1,11 @@
+window.StClientConfig = {
+  "webhookPrefix": "/webhooks",
+  "oauthPrefix": "/oauth",
+  "sessionPrefix": "/session",
+  "apiPrefix": "/api",
+  "snakeApi": true,
+  "superUserApi": "/api/superuser",
+  "adminApi": "/api/admin",
+  "userApi": "/api/user",
+  "publicApi": "/api/public"
+};
