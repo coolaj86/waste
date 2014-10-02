8ead9ca70b9b354f54f0d02cf659efb3d37a54b5
AJ ONeal <awesome@coolaj86.com>
2014-10-01 21:23:12 -0400

    add back client services

--- app/scripts/client-config.js
+++ app/scripts/client-config.js
@@ -8,4 +8,31 @@ window.StClientConfig = {
   "adminApi": "/api/admin",
   "userApi": "/api/user",
   "publicApi": "/api/public"
+, "testProfiles": [
+    { "role": "superuser"
+    , "token": "xxxxxxxx-test-xxxx-xxxx-root-xxxxxx"
+    }
+  , { "role": "admin"
+    , "token": "xxxxxxxx-test-xxxx-xxxx-admin-xxxxxx"
+    }
+  , { "role": "user"
+    , "token": "xxxxxxxx-test-xxxx-xxxx-user-xxxxxxx"
+    }
+  , { "role": "guest"
+    , "token": "xxxxxxxx-test-xxxx-xxxx-guest-xxxxxx"
+    }
+  ]
+, "useSplash": false
+, "stripe": {
+    "publicKey": "pk_test_hwX1wzG4OMEv9esujApHjxI7"
+  , "storeName": "Business Name Here"
+  , "storeLogo": null
+  }
+, "loginProviders": {
+    "facebook": "/facebook/connect"
+  , "twitter": "/twitter/authn/connect"
+  , "tumblr": "/tumblr/connect"
+  , "ldsconnect": "/ldsconnect/connect"
+  , "loopback": "/loopback/connect"
+  }
 };
