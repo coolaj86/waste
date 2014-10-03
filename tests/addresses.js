44d2426d2d2a2d3c6f28bc84b277b82dec3d6841
AJ ONeal <awesome@coolaj86.com>
2014-10-03 03:21:12 -0400

    finally tested addresses

--- tests/addresses.js
+++ tests/addresses.js
@@ -2,37 +2,135 @@
 
 function init(config, DB) {
   var Addrs = require('../lib/account-addresses').createController(config, DB)
+    , Accounts = require('../lib/accounts').createController(config, DB)
     , PromiseA = require('bluebird').Promise
     , tests
+    , shared = {}
     ;
 
   function setup() {
-    return new PromiseA(function (resolve) {
-      resolve();
+    var p
+      ;
+
+    //shared.accId = '310b4365-4359-434f-9224-5421158d7502';
+    if (shared.accId) {
+      console.log('get account');
+      p = Accounts.get(null, shared.accId);
+    } else {
+      p = Accounts.create(config, {});
+    }
+
+    return p.then(function ($acc) {
+      return $acc.load(['addresses']).then(function () {
+        shared.accId = $acc.id;
+        shared.$acc = $acc;
+        return $acc;
+      });
     });
   }
 
   function teardown() {
-    return new PromiseA(function (resolve) {
-      resolve();
+    return PromiseA.resolve();
+  }
+
+  function finalTeardown() {
+    var ps = []
+      ;
+
+    shared.$acc.related('addresses').forEach(function ($addr) {
+      ps.push($addr.destroy());
+    });
+
+    return PromiseA.all(ps).then(function () {
+      return shared.$acc.destroy();
     });
   }
 
+  // TODO test that setting home sets shipping_address_id
   tests = [
-    function addAddress() {
-      return PromiseA.reject(new Error("add address not implemented"));
+    function addAddresses($account) {
+      return Addrs.add(null, $account, $account.related('addresses'), {
+        addressee: "John Doe"
+      , streetAddress: "123 Sesame St"
+      , extendedAddress: ["Claims Office", "Bldg 1 Ste B"]
+      , locality: "Baywatch"
+      , region: "California"
+      , pastalCode: "90210"
+      , countryCode: "US"
+      }).then(function ($addr) {
+        if (1 !== $account.related('addresses').length) {
+          console.error("$account.related('addresses').length");
+          console.error($account.related('addresses').length);
+          throw new Error("should be exactly one address");
+        }
+
+        if ("John Doe" !== $addr.get('addressee')) {
+          throw new Error("Didn't properly create address");
+        }
+      });
     }
-  , function addBillingAddress() {
-      return PromiseA.reject(new Error("add billing not implemented"));
+  , function addBillingAddress($account) {
+      return Addrs.upsertBilling(null, $account, $account.related('addresses'), {
+        addressee: "Mary Jane"
+      , streetAddress: "000 Nowhere Ave"
+      , extendedAddress: null
+      , locality: "Burlington"
+      , region: "Vermont"
+      , pastalCode: "05401"
+      , countryCode: "US"
+      , type: "home"            // should get deleted? or also applied to shipping_address_id?
+      }).then(function ($addr) {
+        if (2 !== $account.related('addresses').length) {
+          console.error("$account.related('addresses').length");
+          console.error($account.related('addresses').length);
+          throw new Error("should be exactly two addresses");
+        }
+
+        if ($addr.get("type")) {
+          console.error('$addr.get("type")');
+          console.error($addr.get("type"));
+          throw new Error("Shouldn't have a type");
+        }
+
+        if (!$account.get("billingAddressId")) {
+          throw new Error("Should have a billing address id on account");
+        }
+
+        shared.addrId = $addr.id; // $addr.get('uuid');
+      });
     }
-  , function addShippingAddress() {
-      return PromiseA.reject(new Error("add shipping not implemented"));
+  , function addShippingAddress($account) {
+      console.log('[4]');
+      console.log('shared.addrId');
+      console.log(shared.addrId);
+      return Addrs.upsertShipping(
+        null
+      , $account
+      , $account.related('addresses')
+      , {}
+      , shared.addrId
+      ).then(function ($addr) {
+        if ("home" !== $addr.get("type")) {
+          console.error('$addr.get("type")');
+          console.error($addr.get("type"));
+          throw new Error("Shouldn't have the type home");
+        }
+
+        if (shared.addrId !== $account.get("shippingAddressId")) {
+          throw new Error("Should have a billing address id on account");
+        }
+
+        delete shared.addrId;
+      });
     }
   , function updateBillingAddress() {
       return PromiseA.reject(new Error("update billing not implemented"));
     }
   , function deleteShippingAddress() {
-      return PromiseA.reject(new Error("update billing not implemented"));
+      return PromiseA.reject(new Error("delete shipping not implemented"));
+    }
+  , function rejectAddress() {
+      return PromiseA.reject(new Error("reject address not implemented"));
     }
   ];
 
@@ -40,6 +138,7 @@ function init(config, DB) {
     tests: tests
   , setup: setup
   , teardown: teardown
+  , finalTeardown: finalTeardown
   };
 }
 
