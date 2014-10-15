c00623a6e8d561b707888bb751cda7d17bc64c45
AJ ONeal <awesome@coolaj86.com>
2014-10-14 23:26:44 -0700

    split angular stuff into own repo

--- app/scripts/controllers/admin.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('yololiumApp')
-  .controller('AdminCtrl', ['$scope', '$http', 'mySession', function ($scope, $http, mySession) {
-    var scope = this
-      ;
-
-    scope.session = mySession;
-  }]);
